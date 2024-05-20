dbba178e22a8678512c8300816bed7cfe3784242
EunJung <98pej11@gmail.com>
2024-05-20 15:41:15 +0900

    [level 0] Title: 배열의 평균값, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,10 +1,3 @@
 const solution = (numbers) => {
-    let answer = 0;
-    
-    for(let i = 0; i< numbers.length; i++){
-        answer += numbers[i];
-    }
-    
-    return answer/numbers.length;
- 
+   return numbers.reduce((acc, cur) => acc + cur) / numbers.length;
 }
