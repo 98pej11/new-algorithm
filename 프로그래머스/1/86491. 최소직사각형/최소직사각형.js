9303154c35777916233907619f8108b0d2b08ccd
EunJung <98pej11@gmail.com>
2024-08-28 17:02:29 +0900

    [level 1] Title: 최소직사각형, Time: 6.40 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,24 +1,13 @@
 function solution(sizes) {
-    let answer = 0;
-    let resMax = 0, resMin = 0;
+    let w = 0;
+    let h = 0;
     
-    for(let i=0;i<sizes.length;i++){
-        let cur = sizes[i];
-        let max, min;
-        
-        if(sizes[i][0] >= sizes[i][1]) {
-            max = sizes[i][0];
-            min = sizes[i][1];
-        }
-        else {
-            max = sizes[i][1];
-            min = sizes[i][0];
-        }
-        if(resMax == 0 || max > resMax) resMax = max;
-        if(resMin == 0 || min > resMin) resMin = min;
-    }
-    
-    answer = resMax * resMin;
-    
-    return answer;
+    sizes.forEach(s => {
+        const [a, b] = s.sort((a,b) => a-b);
+  
+        if (a > h) h = a;
+        if (b > w) w = b;
+    });
+
+    return w * h;
 }
