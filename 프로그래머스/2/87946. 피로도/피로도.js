f42ae8be5a92dc8847ed7f139948cb8bc08608f2
EunJung <98pej11@gmail.com>
2024-09-09 20:27:57 +0900

    [level 2] Title: 피로도, Time: 568.71 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,33 +1,19 @@
 function solution(k, dungeons) {
-  let answer = 0;
-  // 방문했는지 확인하기 위한 배열
-  const visited = Array.from({ length: dungeons.length }, () => false);
+    let answer = 0;
+    const visited = Array.from({ length: dungeons.length }, () => false);
 
-  // 완전탐색을 위한 DFS(남은 피로도, 진행단계)
-  function DFS(hp, L) {
+    dfs(k, 0);
 
-    // 던전의 수 만큼 반복한다.
-    for (let i = 0; i < dungeons.length; i++) {
-
-      // 방문하지 않았고 현재 남은 피로도가 최소 필요도 보다 크거나 같으면 실행
-      if (!visited[i] && dungeons[i][0] <= hp) {
-
-        // 현재 들어온 던전을 방문 처리
-        visited[i] = true;
-
-        // DFS(현재 피로도 - 방문 던전, 진행단계 + 1)
-        DFS(hp - dungeons[i][1], L + 1);
-
-        // DFS 종료 후 방문을 끝내준다.
-        visited[i] = false;
-      }
+    function dfs(remain, value) {
+        for(let i=0;i<dungeons.length;i++) {
+            if(!visited[i] && dungeons[i][0] <= remain) {
+                visited[i] = true; // 방문처리
+                dfs(remain-dungeons[i][1], value+1); // 재귀
+                visited[i] = false; // 초기화
+            }
+        }
+        answer = Math.max(answer,value);
     }
-
-    // 가장 깊이 들어간 진행단계를 answer에 넣어준다.
-    answer = Math.max(answer, L);
-  }
-
-  DFS(k, 0);
-
-  return answer;
+    
+    return answer;
 }
