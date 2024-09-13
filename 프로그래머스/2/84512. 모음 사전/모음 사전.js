f2cdfe855de64a40d97c6e03fb9d0eb883da2c48
EunJung <98pej11@gmail.com>
2024-09-13 13:54:52 +0900

    [level 2] Title: 모음 사전, Time: 6.05 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -0,0 +1,26 @@
+function solution(word) {
+    let answer = 0;
+    let count = 0;
+    let text = "AEIOU";
+    let visited = Array.from({length: word.length}, ()=>false);
+    
+    dfs('', text, 0);
+    
+  
+    function dfs(fixed, list) {
+        if(fixed === word) {
+            console.log(fixed, word);
+            answer = count;
+            return;
+        }
+
+        for(let i=0;i<list.length;i++) {
+            if(!visited[i] && fixed.length < 5) {
+                count++;
+                dfs(fixed + list[i], list);
+            }
+        }
+    }
+    
+    return answer;
+}
