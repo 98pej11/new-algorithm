20e3d0044082c9a5d7f6d0e61e275db008bf287f
EunJung <98pej11@gmail.com>
2024-09-26 19:55:03 +0900

    [level 2] Title: 조이스틱, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.js
@@ -1,32 +1,20 @@
 function solution(name) {
-    // ABCDEFGHIJKLMNOPQRSTUVWXYZ
-    // 가운데: M,N : 77,78
-    // A = 65 / Z = 90
     var answer = 0;
-    let min = name.length - 1;
-    
+    let len = name.length - 1;
+
     for(let i=0;i<name.length;i++) {
-        if(name.charCodeAt(i)<78) {
-            answer += name.charCodeAt(i) - 65;
-        }
-        else{
-            answer += 91 - name.charCodeAt(i);
-        }
+        answer += Math.min(name.charCodeAt(i) - 65, 91 - name.charCodeAt(i));
         
-        let nextIndex = i + 1;
-        
-        while (nextIndex < name.length && name.charCodeAt(nextIndex) === 65) {
-          nextIndex += 1;
+        let idx = i+1;
+
+        while(idx < name.length && name[idx] === 'A') {
+            idx++;
         }
+
+        len = Math.min(len, 2*i + name.length - idx, 2*(name.length - idx) + i);
         
-        min = Math.min(
-          min, // 그대로 쭉갈지
-          i * 2 + name.length - nextIndex,  // 가다가 만나면 뒤로 돌릴지
-          i + (name.length - nextIndex) * 2
-        );
     }
     
-    answer += min;
     
-    return answer;
+    return answer + len;
 }
