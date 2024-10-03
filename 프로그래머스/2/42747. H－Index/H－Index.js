7789c4ac467c3e1d16973df4ec2d79bb69f7da1c
EunJung <98pej11@gmail.com>
2024-10-03 17:18:23 +0900

    [level 2] Title: H-Index, Time: 0.58 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -2,23 +2,16 @@ function solution(citations) {
     var answer = 0;
     
     citations.sort((a,b) => b-a);
-    // [6,5,3,1,0]
-    // [11,10,1]
-    // [4,3]
-    // [0,0,0,0]
-    // index: 6 , 순서: 7번째
-    // index: 4, 순서: 5번째, 9-5 = 4
-    
+
     for(let i=0;i<citations.length;i++) {
-        if(i+1 <= citations[i]) {
-            if(i+1 === citations.length) {
-                answer = i+1;
-                break;
-            }
-            else if(i+1 >= citations[i+1]) {
-                answer = i+1;
-                break;
-            }
+        if(i+1 > citations[i]) continue;
+        if(i+1 === citations.length) {
+            answer = i+1;
+            break;
+        }
+        else if(i+1 >= citations[i+1]) {
+            answer = i+1;
+            break;
         }
     }
     
