43e5f62e4b16b090bcb940a1f42e3aafd11333c0
EunJung <98pej11@gmail.com>
2024-10-04 20:29:34 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,16 +1,16 @@
 function solution(brown, yellow) {
     var answer = [];
-
-    for(let i=1;i<=yellow/2;i++) {
-        let ii = yellow / i;
-        if(yellow % i === 0 && (i+2+ii)*2 === brown) {
-            answer.push(i+2);
-            answer.push(ii+2);
+    
+    for(let i=1;i<=yellow;i++) {
+        if(yellow % i === 0) {
+            let ii = yellow / i;
+            if(i*2 + (ii-2)*2 === brown || (i-2)*2 + ii*2 === brown) {
+                answer.push(i+2);
+                answer.push(ii+2);
+            }
             break;
         }
     }
-    
-    if(yellow === 1) answer = [3,3];
     
     return answer.sort((a,b) => b-a);
 }
