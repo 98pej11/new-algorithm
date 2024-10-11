dcb9109500fc67ce95c8267ebd08be35f0903322
EunJung <98pej11@gmail.com>
2024-10-11 20:38:52 +0900

    [level 2] Title: 타겟 넘버, Time: 80.60 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 38.1 MB, 시간: 291.58 ms
+메모리: 37.9 MB, 시간: 80.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 17일 14:48:22
+2024년 10월 11일 20:38:50
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,30 +1,22 @@
 function solution(numbers, target) {
-    let answer = 0;
-    let N = numbers.length;
-    let signList = [];
+    var answer = 0;
     
-    dfs('', 0);
+    dfs(numbers, 0);
     
-    function dfs (list, count) {
-        if(count === N) {
-            let calcRes = calc(list);
-            if(calcRes === target) answer++;
+    function dfs(list, idx) {
+        if(idx === list.length) {
+            let sum = 0;
+            for(let item of list) {
+                sum += item;
+            }
+            if(sum === target) answer++;
             return;
         }
-        // 배열에 이렇게 추가해도되는지
-        dfs(list+'+', count+1, N);
-        dfs(list+'-', count+1, N);
+
+        dfs(list, idx+1);
+        list[idx] *= -1;
+        dfs(list, idx+1);
     }
     
-    function calc (list) {
-        let res = 0;
-        // N 다 쓸수잇는지
-        for(let i=0;i<N;i++) {
-            if(list[i] === '+') res += numbers[i];
-            else res -= numbers[i];
-        }
-        
-        return res;
-    }
     return answer;
 }
