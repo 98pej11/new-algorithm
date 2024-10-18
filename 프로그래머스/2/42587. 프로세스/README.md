21c3f00f880805f96e8834164a14cdbab1c6ba6a
EunJung <98pej11@gmail.com>
2024-10-18 21:14:52 +0900

    [level 2] Title: 프로세스, Time: 0.75 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587#) 
 
 ### 성능 요약
 
-메모리: 34.8 MB, 시간: 12.48 ms
+메모리: 33.6 MB, 시간: 0.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 21:14:03
+2024년 10월 18일 21:14:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -5,39 +5,25 @@ function solution(priorities, location) {
     const stack = [...priorities];
     
     stack.sort();
- 
-    // console.log(max);
-    
-    // 숫자가 클 수록 우선순위 높음
 
     for(let i=0;i<priorities.length;i++) {
         queue.enqueue(String.fromCharCode(65 + i));
     }
     
-    console.log(queue);
-    
-    // [a,b,c,d] -> queue
-    // [2,1,3,2] -> pro
-    // [1,2,2,3]
-    
     while(queue.size() > 0){
         let idx = queue.storage[queue.front].charCodeAt(0) - 65;
         
-        console.log(idx,priorities[idx],stack[stack.length-1] );
          if(priorities[idx] < stack[stack.length-1]) {
             let item = queue.dequeue();
             queue.enqueue(item);
         }
         else {
-            stack.pop(); // 최댓값 빼기
+            stack.pop();
             answer.push(queue.dequeue());
         };
     }
-
-     console.log(answer);
    
     for(let i=0;i<answer.length;i++) {
-        console.log(String.fromCharCode(65+location));
         if(String.fromCharCode(65+location) === answer[i]) return i+1;
     }
 }
