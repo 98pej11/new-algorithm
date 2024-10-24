9079df32b8d98d15b0b20198676d81fbd470852e
EunJung <98pej11@gmail.com>
2024-10-24 15:34:13 +0900

    [level 2] Title: 구명보트, Time: 47.87 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,54 @@
+function solution(people, limit) {
+    var answer = 0;
+    
+    people.sort((a,b)=>a-b);
+    
+    const q = new Queue();
+    
+    for(let i=0;i<people.length;i++) {
+        q.enqueue(people[i]);
+    }
+    
+    while(q.size() > 0) {
+        let sum = q.pop();
+        
+        if(sum + q.storage[q.front] <= limit) {
+            sum += q.dequeue();
+        }
+        // while(sum + q.storage[q.front] <= limit) {
+        //     sum += q.dequeue();
+        // }
+        answer++;
+    }
+    
+    return answer;
+}
+
+class Queue {
+    constructor() {
+        this.storage = {};
+        this.front = 0;
+        this.rear = 0;
+    }
+    size() {
+        return this.rear - this.front;
+    }
+    enqueue(item) {
+        this.storage[this.rear] = item;
+        this.rear++;
+    }
+    dequeue() {
+        let item = this.storage[this.front];
+        delete this.storage[this.front];
+        this.front++;
+        
+        return item;
+    }
+    pop() {
+        let item = this.storage[this.rear-1];
+        delete this.storage[this.rear-1];
+        this.rear--;
+        
+        return item;
+    }
+}
