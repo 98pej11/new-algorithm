96917d07b8d92659f689c5f748609f7451bb946a
EunJung <98pej11@gmail.com>
2024-10-24 10:48:28 +0900

    [level 2] Title: 주식가격, Time: 6.38 ms, Memory: 45.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,7 +1,6 @@
 function solution(prices) {
     var answer = [];
     
-    // prices의 idx가 시간
     for(let i=0;i<prices.length;i++) {
         let cnt = 0;
         for(let j=i+1;j<prices.length;j++) {
@@ -10,10 +9,6 @@ function solution(prices) {
         }
         answer.push(cnt);
     }
-    
-  
-    // 
-    
     
     return answer;
 }
