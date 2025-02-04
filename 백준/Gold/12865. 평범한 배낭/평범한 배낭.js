822f04a4afb91c839942e30cdd6bf3deb9800bfb
EunJung <98pej11@gmail.com>
2025-02-04 19:59:37 +0900

    [Gold V] Title: 평범한 배낭, Time: 224 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const [N, W] = input[0].split(" ").map(Number);
 
-const dp = Array.from({ length: W + 1 }, () => 0);
+const dp = Array(W + 1).fill(0);
 
 for (let i = 1; i <= N; i++) {
   const [w, v] = input[i].split(" ").map(Number);
