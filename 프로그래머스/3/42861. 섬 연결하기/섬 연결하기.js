52fdecb5ba7ec15b498805d49c94767583090036
EunJung <98pej11@gmail.com>
2025-02-18 13:39:47 +0900

    [level 3] Title: 섬 연결하기, Time: 8.56 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -8,16 +8,18 @@ function solution(N, costs) {
         return parent[x] = find(parent[x]);
     }
 
-    function union(a, b) {
-        const rootA = find(a);
-        const rootB = find(b);
-        if (rootA !== rootB) parent[rootB] = rootA;
-    }
+    // function union(a, b) {
+    //     const rootA = find(a);
+    //     const rootB = find(b);
+    //     if (rootA !== rootB) parent[rootB] = rootA;
+    // }
     
     costs.sort((a,b) => a[2] - b[2]);
+    console.log(costs);
+    
     for(let [u, v, cost] of costs) {
         if (find(u) !== find(v)) {  // 사이클 방지
-            union(u, v);
+            parent[find(v)] = find(u);
             answer += cost;
         }
     }
