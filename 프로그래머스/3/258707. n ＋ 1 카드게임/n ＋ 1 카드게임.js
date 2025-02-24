39b09ac08055974d9907f67dd0ea8b616dd4e23e
EunJung <98pej11@gmail.com>
2025-02-24 12:18:04 +0900

    [level 3] Title: n + 1 카드게임, Time: 6.94 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.js
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.js
@@ -5,31 +5,27 @@ function solution(coin, cards) {
     const nextSet = new Set();
     const N = cards.length+1;
     let idx = 0;
-    
-    console.log(N);
-    
+
     for(let i=0;i<cards.length/3;i++) {
         originSet.add(cards[idx++]);
     }
-    console.log(originSet);
     
     while(idx < cards.length) {
-        // 뒤의 두개중에서 있는지 확인
         nextSet.add(cards[idx++]);
         nextSet.add(cards[idx++]);
         
-        // 일단 버릴 카드 있는지 확인
+        // 기존 카드중 일단 버릴 카드 있는지 확인
         if(isOriginDeleteCard()) {
             round++;
             continue;
         }
-        
+        // 기존 카드 + 새로 추가된 카드 고려해서 버릴 카드 있는지 확인
         if(isMixDeleteCard() && coin >= 1) {
             round++;
             coin--;
             continue;
         }
-             
+        // 새로 추가된 카드중 버릴 카드 있는지 확인
         if(isNextDeleteCard() && coin >= 2) {
             round++;
             coin-=2;
@@ -72,7 +68,5 @@ function solution(coin, cards) {
         return false;
     }
     
-    
-         
     return round;
 }
