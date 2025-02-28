ef29a9de25e09f1c9a86746e80c6193af1fef20d
EunJung <98pej11@gmail.com>
2025-02-28 22:08:53 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.71 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,15 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         Integer[] arr = new Integer[numbers.length];
+        
         for(int i=0; i<numbers.length; i++) arr[i]=numbers[i];
         Arrays.sort(arr, (a,b)->b-a);
-        System.out.println(Arrays.toString(arr));
-        
-        // int num1 = 0;
-        // int num2 = 0;
-        
-        // if(arr[0] >= 0 && arr[1] >= 0) num1 = arr[0]*arr[1];
-        // if(arr[arr.length-1] <= 0 && arr[arr.length - 2] <= 0) num2 = arr[arr.length-1]*arr[arr.length - 2];
+
         return Math.max(arr[0]*arr[1], arr[arr.length-1]*arr[arr.length - 2]);
     }
 }
