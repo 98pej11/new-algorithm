36ec23c3038d614ea59fe42ac18dfa11986ed9c0
EunJung <98pej11@gmail.com>
2025-02-28 16:27:45 +0900

    [level 1] Title: 달리기 경주, Time: 251.15 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,23 @@
+function solution(players, callings) {
+    const idxs = {};
+
+    for (let i = 0; i < players.length; i++) {
+        idxs[players[i]] = i;
+    }
+
+    callings.forEach((player) => {
+
+    const curIdx = idxs[player];
+    const nextplayer = players[curIdx - 1];
+
+    players[curIdx - 1] = player;
+    players[curIdx] = nextplayer;
+
+    idxs[player]--;
+    idxs[players[curIdx]]++;
+        
+  });
+
+  return players;
+    
+}
