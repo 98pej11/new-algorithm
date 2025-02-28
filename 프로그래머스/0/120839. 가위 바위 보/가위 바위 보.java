e6d5cfae02a28e96569509b4bb09d86760743b90
EunJung <98pej11@gmail.com>
2025-02-28 18:17:38 +0900

    [level 0] Title: 가위 바위 보, Time: 1.49 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String rsp) {
+        return rsp.replaceAll("2", "1")
+            .replaceAll("5","2")
+            .replaceAll("0", "5")
+            .replaceAll("1", "0");
+    }
+}
