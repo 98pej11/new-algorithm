d100d9e2100c237d370e5914d1844ebfb65b6671
EunJung <98pej11@gmail.com>
2025-02-28 16:06:02 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 3.88 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        List<int[]> list = new ArrayList<>();
+        
+        int idx = -1;
+        int sortIdx = -1;
+        
+        if(ext.equals("code")) idx = 0;
+        else if(ext.equals("date")) idx = 1;
+        else if(ext.equals("maximum")) idx = 2;
+        else if(ext.equals("remain")) idx = 3;
+        
+        if(sort_by.equals("code")) sortIdx = 0;
+        else if(sort_by.equals("date")) sortIdx = 1;
+        else if(sort_by.equals("maximum")) sortIdx = 2;
+        else if(sort_by.equals("remain")) sortIdx = 3;
+        
+        int aa = sortIdx;
+        
+        for(int i=0;i<data.length;i++) {
+            if(data[i][idx] < val_ext) list.add(data[i]);
+        }
+
+        list.sort((a,b) -> a[aa] - b[aa]);
+        list.toArray();
+
+        int[][] answer = list.toArray(new int[list.size()][4]);
+        return answer;
+    }
+
+}
