6af1221e9e9755a73cd98748e8d8c4e3476ecf3a
EunJung <98pej11@gmail.com>
2025-02-28 14:56:41 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int N = num_list.length;
+        int[] answer = new int[N];
+        
+        // for(int item: num_list) {
+        //     answer
+        // }
+        
+        for(int i=0;i<N;i++) {
+            answer[i] = num_list[N - i -1];
+        }
+        
+        return answer;
+    }
+}
