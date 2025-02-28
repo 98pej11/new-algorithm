7541fcea71cc47d161dbad76955569df487f357a
EunJung <98pej11@gmail.com>
2025-02-28 22:21:13 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        
+        char ch1 = arr[num1];
+        char ch2 = arr[num2];
+        
+        arr[num1] = ch2;
+        arr[num2] = ch1;
+    
+        return new String(arr);
+    }
+}
