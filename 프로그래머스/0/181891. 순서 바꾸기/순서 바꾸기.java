d2bb177ce02454e283edf1266743fc4249d4a8eb
EunJung <98pej11@gmail.com>
2025-02-28 21:16:49 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -2,16 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = {};
-        
         int[] newArr1 = new int[n]; // 앞
         int[] newArr2 = new int[num_list.length-n]; // 뒤
         
         newArr1 = Arrays.copyOfRange(num_list, 0, n);
         newArr2 = Arrays.copyOfRange(num_list, n, num_list.length);
-        
-        System.out.println(Arrays.toString(newArr1));
-        System.out.println(Arrays.toString(newArr2));
         
         for(int i=0;i<newArr2.length;i++) {
             num_list[i] = newArr2[i];
