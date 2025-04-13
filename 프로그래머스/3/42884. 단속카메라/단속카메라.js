8f181ef11cd96f66c61c03441383d4c89b46e2f8
EunJung <98pej11@gmail.com>
2025-04-13 13:07:58 +0900

    [level 3] Title: 단속카메라, Time: 26.38 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -0,0 +1,24 @@
+function solution(routes) {
+    var answer = 0;
+    routes.sort((a,b) => a[0] - b[0]);
+
+    console.log(routes);
+    
+    let camera = 1;
+    let cur = routes[0][1];
+    
+    for(let i=1;i<routes.length;i++) {
+        if(routes[i][0] <= cur) {
+            if(routes[i][1] >= cur) continue;
+            else {
+                cur = routes[i][1];
+            }
+        }
+        else {
+            cur = routes[i][1];
+            camera++;
+        }
+    }
+    
+    return camera;
+}
