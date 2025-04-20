9784701ac56399ce2337484c30abfcb8a890167f
EunJung <98pej11@gmail.com>
2025-04-20 21:39:28 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 2054.02 ms, Memory: 490 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.js
@@ -0,0 +1,24 @@
+function solution(n) {
+    // var answer = 0;
+    let arr = [];
+
+    DFS("", 0, 0); 
+    
+    function DFS(str, open, close) {
+        if (str.length === 2 * n) {
+            arr.push(str);
+            return;
+        }
+
+        if (open < n) {
+            DFS(str + "(", open + 1, close);
+        }
+        if (close < open) {
+            DFS(str + ")", open, close + 1);
+        }
+    }
+    
+    
+    return arr.length;
+}
+
