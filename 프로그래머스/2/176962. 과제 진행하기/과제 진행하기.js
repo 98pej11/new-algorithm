9b00eced9b8cbdafa0295096ec60c7f940b348b4
EunJung <98pej11@gmail.com>
2025-05-05 18:02:27 +0900

    [level 2] Title: 과제 진행하기, Time: 3.09 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -7,8 +7,7 @@ function solution(plans) {
     }
     
     plans.sort((a,b) => a[1]-b[1]);
-    console.log(plans);
-    
+
     let cur = 0;
     let stack = [];
     
@@ -16,42 +15,42 @@ function solution(plans) {
         let [curName, curTime, curPlay] = plans[cur];
         let nextTime = plans[cur+1][1];
         
-        // 다음 시간이 내 플레이 시간을 잡아먹을 경우: 그만큼 빼고 다음거 실행하고 스택에 넣음
+        // 다음 시간이 현제 플레이 종료 전에 맞물릴 경우: 스택에 넣음
         if(curTime + curPlay - nextTime > 0) {
             stack.push([curName,  curPlay - (nextTime - curTime)]);
         }
-        // 다음 시간이 내 플레이 시간과 같을 경우: 결과에 넣고 다음거 실행
+        // 다음 시간이 현제 플레이 종료 시간과 같을 경우: 결과에 넣고 다음 과제 실행
         else if(curTime + curPlay - nextTime === 0) {
             result.push(curName);
         }
-        // console.log(stack);
-        // 다음 시간이 내 플레이 시간보다 적을 경우: 결과에 넣고 스택에 있는거 확인
+        // 다음 시간이 현제 플레이 종료 후에도 시간이 남을 경우: 결과에 넣고 스택에 중지된 과제 확인
         else {
             result.push(curName);
             let remain = nextTime - (curTime + curPlay);
-            // 붕 뜬 시간이 더 클 경우
+            
+            // 붕 뜬 시간이 중지된 과제의 시간보다 더 클 경우
             while(stack.length && remain >= stack[stack.length - 1][1]) {
                 result.push(stack[stack.length - 1][0]);
                 remain -= stack[stack.length - 1][1];
                 stack.pop();
             }
-            // 그게 아니면
+            
             if(stack.length) stack[stack.length - 1][1] -= remain; 
         }
         cur++;
     }
     
+    // 마지막 과제 실행 및 중지된 과제들 최신순으로 실행
     result.push(plans[plans.length-1][0]);
     while(stack.length) {
         result.push(stack[stack.length-1][0]);
         stack.pop();
     }
-
+    
     function convertTime(time) {
       const [h, m] = time.split(":").map(Number);
       return h * 60 + m;
     }
-
     
     return result;
 }
