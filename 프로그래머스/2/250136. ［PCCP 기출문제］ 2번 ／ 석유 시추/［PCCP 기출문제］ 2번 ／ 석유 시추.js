c2bd5bbcb3397dd05035dfa23ef27ee9e6b5a5ae
EunJung <98pej11@gmail.com>
2025-05-05 15:26:40 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 144.29 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -0,0 +1,65 @@
+function solution(land) {
+    let dir = [[0,1], [0,-1], [1,0], [-1,0]];
+    let queue = [];
+    let list = [];
+    
+    let N = land.length;
+    let M = land[0].length;
+    let result = Array(M).fill(0);
+    
+    for(let i=0;i<N;i++) {
+        for(let j=0;j<M;j++) {
+            if(land[i][j] === 1) {
+                list = [];
+                land[i][j] = -1;
+                queue.push([i,j]);
+                list.push([i,j]);
+                
+                let cost = BFS();
+                let visited = Array(M).fill(false);
+                for(let item of list) {
+                    let r = item[0];
+                    let c = item[1];
+                    
+                    if(!visited[c]) {
+                        visited[c] = true;
+                        result[c] += cost;
+                    }
+                    
+                    land[r][c] = cost;
+                }
+            }
+        }
+    }
+    
+    console.log(land);
+    let max = 0;
+    
+    for(let i=0;i<M;i++) {
+        max = Math.max(max, result[i]);
+    }
+    
+    return max;
+    
+    function BFS(){
+       let cost = 1;
+        
+       while(queue.length) {
+         let cur = queue.pop();
+
+         for(let [dr,dc] of dir) {
+           let nr = cur[0]+dr;
+           let nc = cur[1]+dc;
+          
+           if(nr >=0 && nr < N && nc >=0 && nc < M && land[nr][nc] === 1) {
+               land[nr][nc] = -1;
+               cost++;
+               queue.push([nr,nc]);
+               list.push([nr,nc]);
+           }
+         }
+       }
+        
+       return cost;
+    } 
+}
