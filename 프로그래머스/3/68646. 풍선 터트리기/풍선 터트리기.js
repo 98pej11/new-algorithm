959e44e2f03b16c9b2f868f98536b444ab1fc7cc
EunJung <98pej11@gmail.com>
2025-05-13 17:44:04 +0900

    [level 3] Title: 풍선 터트리기, Time: 429.96 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.js
@@ -0,0 +1,38 @@
+// 4시 48분
+function solution(a) {
+    const MAX = 1000000000;
+    let leftMin = MAX;
+    let rightMin = MAX;
+    let count = 0;
+    
+    let leftSet = new Set();
+    let rightSet = new Set();
+    
+    for(let i=0;i<a.length;i++) {
+        if(i !== 0) {
+            leftMin = Math.min(leftMin, a[i-1]);
+        }
+        
+        if(a[i] > leftMin) leftSet.add(i);
+    }
+    
+    for(let i=a.length - 1;i>=0;i--) {
+        if(i !== a.length - 1) {
+            rightMin = Math.min(rightMin, a[i+1]);
+        }
+
+        if(a[i] > rightMin) rightSet.add(i);
+    }
+    
+    for(let i=0;i<a.length;i++) {
+        if(leftSet.has(i) && rightSet.has(i)) count++;
+    }
+    
+    return a.length - count;
+}
+// 작은거 최대 하나 없앨 수 있음, 나머지는 다 둘중 큰거
+// 큰게 더 많아야 되는건데?
+// [-16,27,65,-2,58,-92,-71,-68,-61,-33]
+// 큰것들을 다 없앳을때, 양쪽이 다 나보다 작은 수면, 불가
+
+// -
