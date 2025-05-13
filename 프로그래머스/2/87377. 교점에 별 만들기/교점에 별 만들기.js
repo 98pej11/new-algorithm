05176c68f4e380e4f839ad955c7020f81e814d15
EunJung <98pej11@gmail.com>
2025-05-13 18:56:15 +0900

    [level 2] Title: 교점에 별 만들기, Time: 100.43 ms, Memory: 64.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.js
@@ -0,0 +1,50 @@
+function solution(line) {
+    const N = line.length;
+    const set = new Set();
+    const result = [];
+    
+    for(let i=0;i<N;i++) {
+        let [A,B,E] = line[i];
+        for(let j=i+1;j<N;j++) {
+            let [C,D,F] = line[j];
+
+            let x = (B*F - E*D) / (A*D - B*C);
+            let y = (E*C - A*F) / (A*D - B*C);
+            
+            if(Number.isInteger(x) && Number.isInteger(y)) set.add(x+" "+y);
+        }
+    }
+    let rMin = Infinity;
+    let rMax = -Infinity;
+    let cMin = Infinity;
+    let cMax = -Infinity;
+    
+    for(let item of set) {
+        let [r,c] = item.split(" ");
+        rMin = Math.min(rMin, r);
+        rMax = Math.max(rMax, r);
+        cMin = Math.min(cMin, c);
+        cMax = Math.max(cMax, c);
+    }
+    console.log(set);
+    console.log(rMin, rMax, cMin, cMax);
+    
+    for(let i=cMax;i>=cMin;i--) {
+        let str = "";
+        
+        for(let j=rMin;j<=rMax;j++) {
+            if(set.has(j+" "+i)) str+='*';
+            else str+='.';
+        }
+        
+        result.push(str);
+    }
+    
+    return result;
+}
+
+// 2x-y+4 = -2x-y+4
+// 4x = 0
+
+// 2x-y+4 = 5x+8y+12
+// 3x + 9y = -8
