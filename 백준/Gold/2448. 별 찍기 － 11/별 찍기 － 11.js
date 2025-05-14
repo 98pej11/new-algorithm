a3be231a68455a906ecfc7c6ac9c7c5f845b48dc
EunJung <98pej11@gmail.com>
2025-05-14 13:09:09 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 2248 ms, Memory: 197184 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = Number(input[0]);
+const star = Array.from({ length: N }, () => Array.from({ length: 2 * N - 1 }, () => " "));
+
+inputStar(0, N - 1, N);
+
+for (let i = 0; i < N; i++) {
+  let str = "";
+  for (let j = 0; j < 2 * N - 1; j++) {
+    str += star[i][j];
+  }
+  console.log(str);
+}
+
+// 0 23 24
+function inputStar(r, c, N) {
+  if (N === 3) {
+    star[r][c] = "*";
+    star[r + 1][c - 1] = "*";
+    star[r + 1][c + 1] = "*";
+    star[r + 2][c - 2] = "*";
+    star[r + 2][c - 1] = "*";
+    star[r + 2][c] = "*";
+    star[r + 2][c + 1] = "*";
+    star[r + 2][c + 2] = "*";
+  } else {
+    inputStar(r, c, N / 2);
+    inputStar(r + N / 2, c - N / 2, N / 2);
+    inputStar(r + N / 2, c + N / 2, N / 2);
+  }
+}
