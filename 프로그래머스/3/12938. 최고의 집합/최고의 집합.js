1cf117e2063cc29f2107234e52cee134ecbc0b6b
EunJung <98pej11@gmail.com>
2025-05-15 22:04:40 +0900

    [level 3] Title: 최고의 집합, Time: 0.47 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.js
@@ -1,10 +1,4 @@
 function solution(n, s) {
-    if (s < n) return [-1]; // 자연수니까 1 이상은 되어야 함
-
-    const base = Math.floor(s / n); // 기본값
-    const remainder = s % n; // 나머지를 얼마나 더해줘야 하나
-
-    // n - remainder 개는 base, remainder 개는 base + 1
-    const result = Array(n - remainder).fill(base).concat(Array(remainder).fill(base + 1));
-    return result.sort((a, b) => a - b);
+    var answer = [];
+    return answer;
 }
