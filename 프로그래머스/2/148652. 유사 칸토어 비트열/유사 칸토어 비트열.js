fa579b9b6b677c6dd7d2e16f3c3e716c710eb2f8
EunJung <98pej11@gmail.com>
2025-05-16 13:53:16 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 481.35 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.js
@@ -1,17 +1,4 @@
 function solution(n, l, r) {
-    let answer = 0;
-
-    for (let i = l - 1; i < r; i++) {
-        answer += isOne(i);
-    }
-
-    function isOne(index) {
-        while (index > 0) {
-            if (index % 5 === 2) return 0; // 가운데 구간이면 무조건 0
-            index = Math.floor(index / 5); // 상위 레벨로 이동
-        }
-        return 1;
-    }
-
+    var answer = 0;
     return answer;
 }
