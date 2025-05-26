20006a8d60d97204b7f5058fca02fe6ba5cfc77a
EunJung <98pej11@gmail.com>
2025-05-26 23:13:50 +0900

    [level 3] Title: 불량 사용자, Time: 93.15 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,4 +1,40 @@
 function solution(user_id, banned_id) {
-    var answer = 0;
-    return answer;
+    let result = [];
+    let set = new Set();
+    
+    for(let item of banned_id) {
+        let ids = [];
+        
+        for(let id of user_id) {
+            if(item.length !== id.length) continue;
+            let tf = true;
+            for(let i = 0; i < id.length; i++) {
+                if(item[i] === '*') continue;
+                if(item[i] !== id[i]) {
+                    tf = false;
+                    break;
+                }
+            }
+            if(tf) ids.push(id);
+        }
+        result.push(ids);
+    }
+    
+    DFS(new Set(), 0);
+    
+    function DFS(curSet, index) {
+        if(index === banned_id.length) {
+            set.add([...curSet].sort().join(','));
+            return;
+        }
+
+        for(let user of result[index]) {
+            if(curSet.has(user)) continue;
+            curSet.add(user);
+            DFS(curSet, index + 1);
+            curSet.delete(user);
+        }
+    }
+
+    return set.size;
 }
