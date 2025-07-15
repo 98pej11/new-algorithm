323f6f3ad0f001cefa9ecf0ecdc1b611dec778ec
EunJung <98pej11@gmail.com>
2025-07-15 13:53:21 +0900

    [Gold IV] Title: 전구와 스위치, Time: 200 ms, Memory: 17284 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.js
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.js
@@ -5,31 +5,10 @@ const N = parseInt(input[0]);
 const originInput = input[1].split("");
 const targetInput = input[2].split("");
 
-const INF = Number.MAX_SAFE_INTEGER;
+const INF = Infinity;
 
 const origin = Array.from({ length: N + 2 }, (_, i) => originInput[i - 1] === "1");
 const target = Array.from({ length: N + 2 }, (_, i) => targetInput[i - 1] === "1");
-
-function pushSwitch(idx, lightBulbs) {
-  lightBulbs[idx - 1] = !lightBulbs[idx - 1];
-  lightBulbs[idx] = !lightBulbs[idx];
-  lightBulbs[idx + 1] = !lightBulbs[idx + 1];
-}
-
-function isSame(lightBulbs) {
-  return lightBulbs.slice(1, N + 1).every((state, i) => state === target[i + 1]);
-}
-
-function getResult(lightBulbs) {
-  let count = 0;
-  for (let i = 2; i <= N; i++) {
-    if (lightBulbs[i - 1] !== target[i - 1]) {
-      pushSwitch(i, lightBulbs);
-      count++;
-    }
-  }
-  return isSame(lightBulbs) ? count : INF;
-}
 
 // 첫 번째 버튼을 누르지 않은 경우
 let result1 = getResult([...origin]);
@@ -40,3 +19,24 @@ pushSwitch(1, copied);
 let result2 = getResult(copied) + 1;
 
 console.log(Math.min(result1, result2) === INF ? -1 : Math.min(result1, result2));
+
+function pushSwitch(idx, lights) {
+  lights[idx - 1] = !lights[idx - 1];
+  lights[idx] = !lights[idx];
+  lights[idx + 1] = !lights[idx + 1];
+}
+
+function isSame(lights) {
+  return lights.slice(1, N + 1).every((state, i) => state === target[i + 1]);
+}
+
+function getResult(lights) {
+  let count = 0;
+  for (let i = 2; i <= N; i++) {
+    if (lights[i - 1] !== target[i - 1]) {
+      pushSwitch(i, lights);
+      count++;
+    }
+  }
+  return isSame(lights) ? count : INF;
+}
