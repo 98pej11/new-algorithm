be79d887ba7ccc85c7fe8a3518d45d5c4a3bb0c2
EunJung <98pej11@gmail.com>
2025-07-22 10:31:16 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 14.15 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,32 +1,37 @@
 function solution(sequence) {
-    const n = sequence.length;
-    
-    // 펄스 수열 두 가지 경우를 위한 변수
-    let maxSum1 = -Infinity;
-    let maxSum2 = -Infinity;
+    let max1 = -Infinity;
+    let max2 = -Infinity;
 
-    // 현재까지의 연속 펄스 부분수열 합
-    let currSum1 = 0;
-    let currSum2 = 0;
+    let sum1 = 0;
+    let sum2 = 0;
 
-    for (let i = 0; i < n; i++) {
-        // i가 짝수면 [1, -1, 1, -1...] 형태에서 1
-        const pulse1 = i % 2 === 0 ? 1 : -1;
-        // i가 짝수면 [-1, 1, -1, 1...] 형태에서 -1
-        const pulse2 = i % 2 === 0 ? -1 : 1;
+    for (let i = 0; i < sequence.length; i++) {
+        let key1 = i % 2 === 0 ? 1 : -1; // [1,-1,..]
+        let key2 = i % 2 === 0 ? -1 : 1; // [-1,1,..]
 
-        // 원소에 펄스 적용
-        const val1 = sequence[i] * pulse1;
-        const val2 = sequence[i] * pulse2;
+        let multi1 = sequence[i] * key1;
+        let multi2 = sequence[i] * key2;
 
-        // Kadane's algorithm 적용
-        currSum1 = Math.max(val1, currSum1 + val1);
-        currSum2 = Math.max(val2, currSum2 + val2);
+        if (sum1 + multi1 > multi1) {
+            sum1 += multi1;
+        } else {
+            sum1 = multi1;
+        }
 
-        // 최대값 갱신
-        maxSum1 = Math.max(maxSum1, currSum1);
-        maxSum2 = Math.max(maxSum2, currSum2);
+        if (sum1 > max1) {
+            max1 = sum1;
+        }
+
+        if (sum2 + multi2 > multi2) {
+            sum2 += multi2;
+        } else {
+            sum2 = multi2;
+        }
+
+        if (sum2 > max2) {
+            max2 = sum2;
+        }
     }
 
-    return Math.max(maxSum1, maxSum2);
+    return Math.max(max1, max2);
 }
